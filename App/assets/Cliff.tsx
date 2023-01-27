414d41e77ff705c9d69baf6260709c8b053f8ed9
SergiMS03 <a19sermelseg@inspedralbes.cat>
2023-01-27 10:14:18 +0100

    Carga el tiled map y el actor

---
+++ App/assets/Cliff.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Cliff" tilewidth="16" tileheight="16" tilecount="63" columns="7">
+ <image source="../colisionMap/MiniWorldSprites/Ground/Cliff.png" width="112" height="144"/>
+ <tile id="42">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
--- App/core/src/com/tenarse/game/screens/GameScreen.java
+++ App/core/src/com/tenarse/game/screens/GameScreen.java
@@ -8,6 +8,10 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -20,17 +24,38 @@ public class GameScreen implements Screen {
 
     private Stage stage;
     private Jugador jugador;
-    private Background background;
 
-    private ShapeRenderer shapeRenderer;
-    private Batch batch;
+    private TiledMap map;
+
+    private TmxMapLoader mapLoader;
+
+    private OrthographicCamera camera;
+
+    private OrthogonalTiledMapRenderer renderer;
+
+    private int tileWidth, tileHeight,
+            mapWidthInTiles, mapHeightInTiles,
+            mapWidthInPixels, mapHeightInPixels;
 
     public GameScreen(Batch prevBatch, Viewport prevViewport){
-        shapeRenderer = new ShapeRenderer();
 
-        background = new Background(3840, 2160);
-        jugador = new Jugador(background.getWidth() / 2, background.getHeight() / 2, Settings.PLAYER_WIDTH, Settings.PLAYER_HEIGHT);
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("assets/colisionableMap.tmx");
+        MapProperties properties = map.getProperties();
+        tileWidth         = properties.get("tilewidth", Integer.class);
+        tileHeight        = properties.get("tileheight", Integer.class);
+        mapWidthInTiles   = properties.get("width", Integer.class);
+        mapHeightInTiles  = properties.get("height", Integer.class);
+        mapWidthInPixels  = mapWidthInTiles  * tileWidth;
+        mapHeightInPixels = mapHeightInTiles * tileHeight;
 
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        prevViewport.setCamera(camera);
+
+
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        jugador = new Jugador(mapWidthInPixels / 2, mapHeightInPixels / 2, Settings.PLAYER_WIDTH, Settings.PLAYER_HEIGHT, false);
 
         //Crear stage
         stage = new Stage(prevViewport, prevBatch);
@@ -38,12 +63,11 @@ public class GameScreen implements Screen {
             stage.getViewport().setWorldSize(stage.getViewport().getWorldWidth() / 2, stage.getViewport().getWorldHeight() / 2);
             stage.getViewport().apply();
         }
-        batch = stage.getBatch();
 
         //Añadir Actores
-        stage.addActor(background);
-        stage.addActor(jugador);
         jugador.setName("jugador");
+        stage.addActor(jugador);
+
 
         //Gestor d'entrada la classe InputHandler
         Gdx.input.setInputProcessor(stage);
@@ -57,27 +81,31 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(.5f, .7f, .9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        System.out.println(stage.getViewport().getCamera().position);
         cameraMapPosition();
+        System.out.println(camera.position.x);
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
         stage.draw();
         stage.act(delta);
     }
 
     private void cameraMapPosition() {
         if (Gdx.app.getType() == Application.ApplicationType.Android){
-            if(jugador.getCollisionRectPlayer().x < (background.getWidth() - (Gdx.graphics.getWidth() / 2) / 2) && jugador.getCollisionRectPlayer().x > (Gdx.graphics.getWidth() / 2) / 2){
-                stage.getViewport().getCamera().position.set(jugador.getCollisionRectPlayer().x, stage.getViewport().getCamera().position.y, 0);
+            if(jugador.getCollisionRectPlayer().x < (mapWidthInPixels - (Gdx.graphics.getWidth() / 2) / 2) && jugador.getCollisionRectPlayer().x > (Gdx.graphics.getWidth() / 2) / 2){
+                camera.position.x = jugador.getCollisionRectPlayer().x;
             }
-            if(jugador.getCollisionRectPlayer().y < (background.getHeight() - (Gdx.graphics.getHeight() / 2) / 2) && jugador.getCollisionRectPlayer().y > (Gdx.graphics.getHeight() / 2) / 2) {
-                stage.getViewport().getCamera().position.set(stage.getViewport().getCamera().position.x, jugador.getCollisionRectPlayer().y, 0);
+            if(jugador.getCollisionRectPlayer().y < (mapHeightInPixels - (Gdx.graphics.getHeight() / 2) / 2) && jugador.getCollisionRectPlayer().y > (Gdx.graphics.getHeight() / 2) / 2) {
+                camera.position.y = jugador.getCollisionRectPlayer().y;
             }
         } else {
-            if(jugador.getCollisionRectPlayer().x < (background.getWidth() - (Gdx.graphics.getWidth() / 2)) && jugador.getCollisionRectPlayer().x > (Gdx.graphics.getWidth() / 2)){
-                stage.getViewport().getCamera().position.set(jugador.getCollisionRectPlayer().x, stage.getViewport().getCamera().position.y, 0);
+            if(jugador.getCollisionRectPlayer().x < (mapWidthInPixels - (Gdx.graphics.getWidth() / 2)) && jugador.getCollisionRectPlayer().x > (Gdx.graphics.getWidth() / 2)){
+                camera.position.x = jugador.getCollisionRectPlayer().x;
             }
-            if(jugador.getCollisionRectPlayer().y < (background.getHeight() - (Gdx.graphics.getHeight() / 2)) && jugador.getCollisionRectPlayer().y > (Gdx.graphics.getHeight() / 2)) {
-                stage.getViewport().getCamera().position.set(stage.getViewport().getCamera().position.x, jugador.getCollisionRectPlayer().y, 0);
+            if(jugador.getCollisionRectPlayer().y < (mapHeightInPixels - (Gdx.graphics.getHeight() / 2)) && jugador.getCollisionRectPlayer().y > (Gdx.graphics.getHeight() / 2)) {
+                camera.position.y = jugador.getCollisionRectPlayer().y;
             }
         }
 
