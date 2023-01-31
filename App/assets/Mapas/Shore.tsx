0321555cb925392df90aa75580ffc798238bca0d
SergiMS03 <a19sermelseg@inspedralbes.cat>
2023-01-31 17:32:58 +0100

    Mapa modificado

---
+++ App/assets/Mapas/Shore.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Shore" tilewidth="16" tileheight="16" tilecount="5" columns="5">
+ <image source="MiniWorldSprites/Ground/Shore.png" width="80" height="16"/>
+ <tile id="1">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="colisionable" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
--- App/assets/Mapas/TexturedGrass.tsx
+++ App/assets/Mapas/TexturedGrass.tsx
@@ -1,4 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.0" name="TexturedGrass" tilewidth="16" tileheight="16" tilecount="6" columns="3">
  <image source="MiniWorldSprites/Ground/TexturedGrass.png" width="48" height="32"/>
+ <tile id="4" probability="0.05"/>
+ <tile id="5" probability="0.05"/>
 </tileset>
