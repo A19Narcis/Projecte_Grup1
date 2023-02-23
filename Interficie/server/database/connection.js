89e22cdf9ab3bbaec750888253a664fb02f84ffe
Ins. Ausiàs March v180704 <a19nargomcar@inspedralbes.cat>
2023-02-23 09:23:56 +0100

    New file schemas + campo URL

--- Interficie/server/database/connection.js
+++ Interficie/server/database/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const autoIncrement = require("mongoose-auto-increment")
+const { Partida, Stats } = require("./schemas")
 
 autoIncrement.initialize(mongoose.connection)
 const URL = 'mongodb://a19nargomcar2:paco1234@labs.inspedralbes.cat:7010/?tls=false&authMechanism=DEFAULT&authSource=DAMA_Grup1'
@@ -8,76 +9,7 @@ const options = {
     dbName: 'DAMA_Grup1',
 };
 
-
-mongoose.set('strictQuery', false);
-
 main().catch(err => console.log(err));
-
-const statsPersonatges = new mongoose.Schema({
-    nombreTipo: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    cantidadMinuto: {
-        type: Number,
-    },
-    velocidad: {
-        type: Number,
-        default: 1
-    },
-    fuerza: {
-        type: Number,
-        default: 1
-    },
-    vida: {
-        type: Number,
-        default: 1
-    },
-    armadura: {
-        type: Number
-    }
-})
-
-const jugadorSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    tipo: {
-        type: String,
-        required: true
-    },
-    kills: {
-        type: Number,
-        default: 0
-    }
-})
-
-const partidaSchema = new mongoose.Schema({
-    idP: {
-        type: Number,
-        unique: true,
-        required: true,
-        autoIncrement: true
-    },
-    jugadores: [jugadorSchema],
-    tiempo: {
-        type: String,
-        required: true
-    },
-    puntos: {
-        type: Number,
-        default: 0
-    }
-});
-
-
-
-partidaSchema.plugin(autoIncrement.plugin, { model: 'Partida', field: 'idP'})
-const Partida = mongoose.model('Partida', partidaSchema);
-
-const Stats = mongoose.model('Stats', statsPersonatges);
 
 
 async function main() {
@@ -97,8 +29,8 @@ async function main() {
 }
 
 
-
 module.exports = { 
     Partida,
     Stats
 }
+
---
+++ Interficie/server/database/schemas.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose")
+const autoIncrement = require("mongoose-auto-increment")
+
+autoIncrement.initialize(mongoose.connection)
+
+mongoose.set('strictQuery', false);
+
+const statsPersonatges = new mongoose.Schema({
+    nombreTipo: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    cantidadMinuto: {
+        type: Number,
+    },
+    velocidad: {
+        type: Number,
+        default: 1
+    },
+    fuerza: {
+        type: Number,
+        default: 1
+    },
+    vida: {
+        type: Number,
+        default: 1
+    },
+    armadura: {
+        type: Number
+    },
+    url:{
+        type: String,
+        required: true
+    }
+})
+
+const jugadorSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    tipo: {
+        type: String,
+        required: true
+    },
+    kills: {
+        type: Number,
+        default: 0
+    }
+})
+
+const partidaSchema = new mongoose.Schema({
+    idP: {
+        type: Number,
+        unique: true,
+        required: true,
+        autoIncrement: true
+    },
+    jugadores: [jugadorSchema],
+    tiempo: {
+        type: String,
+        required: true
+    },
+    puntos: {
+        type: Number,
+        default: 0
+    }
+});
+
+partidaSchema.plugin(autoIncrement.plugin, { model: 'Partida', field: 'idP'})
+const Partida = mongoose.model('Partida', partidaSchema);
+
+const Stats = mongoose.model('Stats', statsPersonatges);
+
+
+module.exports = { 
+    Partida,
+    Stats
+}
